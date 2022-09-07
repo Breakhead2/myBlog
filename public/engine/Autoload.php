ff01f4802ef6dc1a7fbc68ed1e52e2d89e8226e9
Denis Sazonov <greenfish@list.ru>
2022-09-07 21:25:27 +0400

    07-09-22_part2

--- public/engine/Autoload.php
+++ public/engine/Autoload.php
@@ -5,7 +5,11 @@ namespace myblog\engine;
 class Autoload
 {
     public function loadClasses($className) {
-        $className = str_replace(['myblog\\', '\\'], ['./', '/'], $className);
-        include "{$className}.php";
+
+        $className = str_replace(['myblog\\', '\\'], ['./', '/'], $className) . ".php";
+
+        if(file_exists($className)){
+            include $className;
+        }
     }
 }
