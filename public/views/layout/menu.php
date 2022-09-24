816d7d1f81ce75c35bb6ec0ef3f0fab00d8cb8e3
Denis Sazonov <greenfish@list.ru>
2022-09-24 19:30:38 +0400

    new branch

--- public/views/layout/menu.php
+++ public/views/layout/menu.php
@@ -5,7 +5,6 @@
         <li class="menu__list"><a class="menu__list__link" href="/photos">Фотографии</a></li>
         <li class="menu__list"><a class="menu__list__link" href="/about_us">О нас</a></li>
         <li class="menu__list"><a class="menu__list__link" href="/login">Войти</a></li>
-        <li class="menu__list"><a class="menu__list__link" href="/login">Войти</a></li>
     </ul>
     <div class="burger__menu">
         <div class="space">
