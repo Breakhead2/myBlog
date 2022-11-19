c3f6537fb051124fd16e3acfd80479eec0dcc862
Denis Sazonov <greenfish@list.ru>
2022-11-19 13:51:23 +0400

    create gallery css&html

--- public/views/photos.php
+++ public/views/photos.php
@@ -1,5 +1,39 @@
 <section id="gallery">
     <div class="container">
-        Здесь будет фото галлерея
+        <div class="gallery_box">
+            <div class="row">
+                <div class="image-container first">
+                    <img src="/src/images/gallery/image_1.jpg" alt="picture">
+                </div>
+                <div class="image-container ">
+                    <img src="/src/images/gallery/image_2.jpg" alt="picture">
+                </div>
+                <div class="image-container">
+                    <img src="/src/images/gallery/image_3.jpg" alt="picture">
+                </div>
+            </div>
+            <div class="row">
+                <div class="image-container">
+                    <img src="/src/images/gallery/image_4.jpg" alt="picture">
+                </div>
+                <div class="image-container">
+                    <img src="/src/images/gallery/image_5.jpg" alt="picture">
+                </div>
+                <div class="image-container last">
+                    <img src="/src/images/gallery/image_6.jpg" alt="picture">
+                </div>
+            </div>
+            <div class="row">
+                <div class="image-container first">
+                    <img src="/src/images/gallery/image_7.jpg" alt="picture">
+                </div>
+                <div class="image-container">
+                    <img src="/src/images/gallery/image_8.jpg" alt="picture">
+                </div>
+                <div class="image-container">
+                    <img src="/src/images/gallery/image_9.jpg" alt="picture">
+                </div>
+            </div>
+        </div>
     </div>
 </section>
